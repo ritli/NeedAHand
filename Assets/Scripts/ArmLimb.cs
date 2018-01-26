6c5fec957996ba95fe355d01a441f985c1245666
ritli <agnar_oskarsson@hotmail.com>
2018-01-26 21:43:11 +0100

    Updated body, throwing arms etc

--- Assets/Scripts/ArmLimb.cs
+++ Assets/Scripts/ArmLimb.cs
@@ -6,7 +6,10 @@ public class ArmLimb : Limb {
 
 	// Use this for initialization
 	void Start () {
-		
+		if (transform.parent)
+        {
+            GetComponent<HingeJoint2D>().connectedBody = transform.parent.GetComponent<Rigidbody2D>();
+        }
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -11,6 +11,10 @@ public class Body : MonoBehaviour {
     //Force added per leg
     public float jumpMultiplier = 50;
 
+    public float throwBaseForce = 10;
+    //Force added per arm
+    public float throwMultiplier = 5;
+
     public float xSpeed = 10;
 
     Rigidbody2D rigidbody;
@@ -33,26 +37,71 @@ public class Body : MonoBehaviour {
     {
         float xVelocity = Input.GetAxis("Horizontal");
 
-        rigidbody.AddForce(xVelocity * transform.right * xSpeed, ForceMode2D.Force);
+        rigidbody.AddForce(xVelocity * Vector2.right * xSpeed, ForceMode2D.Force);
 
         if (Input.GetButtonDown("Vertical"))
         {
             rigidbody.AddForce(Vector2.up * (baseJumpForce + GetLegCount * jumpMultiplier), ForceMode2D.Impulse);
         }
 
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            AddLimb(armPrefab.GetComponent<Limb>());
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            AddLimb(legPrefab.GetComponent<Limb>());
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            ThrowLimb(LimbType.Arm);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ThrowLimb(LimbType.Leg);
+        }
     }
 
-    void AddLimb(Limb limb)
+    bool RemoveLimb(LimbType limbtype)
+    {
+        for (int i = 0; i < limbs.Count; i++)
+        {
+            if (limbs[i].getLimb() == limbtype)
+            {
+                limbs.RemoveAt(i);
+
+                for (int t = 0; t < transform.childCount; t++)
+                {
+                    if (transform.GetChild(i).GetComponent<Limb>().getLimb() == limbtype)
+                    {
+                        Destroy(transform.GetChild(i).gameObject);
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void ThrowLimb(LimbType limbtype)
     {
         GameObject objectToSpawn = null;
 
-        limbs.Add(limb);
+        //If player does not have limb of this type no limb is thrown
+        if (!RemoveLimb(limbtype))
+        {
+            print("No limb left of this type");
 
-        switch (limb.getLimb())
+            return;
+        }
+
+        switch (limbtype)
         {
             case LimbType.Arm:
-                objectToSpawn = armPrefab;                
-                
+                objectToSpawn = armPrefab;
+
                 break;
             case LimbType.Leg:
                 objectToSpawn = legPrefab;
@@ -61,6 +110,51 @@ public class Body : MonoBehaviour {
             default:
                 break;
         }
+
+         GameObject launchedLimb = Instantiate(objectToSpawn, (Vector3)Random.insideUnitCircle * 0.25f + transform.position, Quaternion.Euler(0, 0, Random.Range(0, 360)));
+
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        launchedLimb.GetComponent<Rigidbody2D>().AddForce(dir.normalized * (throwBaseForce + throwMultiplier * GetArmCount), ForceMode2D.Impulse);
+        Destroy(launchedLimb.GetComponent<HingeJoint2D>());
+
+    }
+
+    void AddLimb(Limb limb)
+    {
+        GameObject objectToSpawn = null;
+
+        limbs.Add(limb);
+        bool isTrigger = true;
+
+
+        switch (limb.getLimb())
+        {
+            case LimbType.Arm:
+                objectToSpawn = armPrefab;                
+                
+                break;
+            case LimbType.Leg:
+                isTrigger = false;
+                objectToSpawn = legPrefab;
+
+                break;
+            default:
+                break;
+        }
+
+        GameObject g = Instantiate(objectToSpawn, (Vector3)Random.onUnitSphere + transform.position, Quaternion.Euler(0, 0, Random.Range(0, 360)), transform);
+
+
+        Vector3 offset = transform.position - g.transform.position;
+
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+
+
+        g.transform.rotation = Quaternion.Euler(0, 0, angle + 90);
+
+        g.GetComponent<Rigidbody2D>().gravityScale = 0;
+        g.GetComponent<Collider2D>().isTrigger = isTrigger;
     }
 
 
