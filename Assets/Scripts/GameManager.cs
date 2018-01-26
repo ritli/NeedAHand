b65f91e3b6b80ca961354eef72aa80ff25e79495
Gustaf Andersson <c.gustaf@hotmail.se>
2018-01-26 20:28:31 +0100

    GM skelett

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager
+{
+    #region public
+    // Functions
+    public static void init()
+    {
+
+    }
+    
+    // Properties
+    #endregion
+
+    #region private
+    private static List<Body> m_players = new List<Body>();
+    private static object m_currentMap;
+    #endregion
+}
