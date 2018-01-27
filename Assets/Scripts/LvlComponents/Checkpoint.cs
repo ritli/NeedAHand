20cbfe323d752eae15cc2e19804ca42a0a854bd0
Gustaf Andersson <c.gustaf@hotmail.se>
2018-01-27 14:57:41 +0100

    sdfgwq

--- Assets/Scripts/LvlComponents/Checkpoint.cs
+++ Assets/Scripts/LvlComponents/Checkpoint.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class Checkpoint : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameManager._GetInstance().SetCheckpoint(gameObject);
+    }
 }
